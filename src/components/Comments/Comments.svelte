09bd4007804e741dcb43c75deca4fb9be198b44c
Mike Peiman <mikepeiman@gmail.com>
2021-04-09 18:35:18 -0400

    further developed Comments comopnent, displaying comment replies, formatting date-time

--- src/components/Comments/Comments.svelte
+++ src/components/Comments/Comments.svelte
@@ -1,40 +1,66 @@
 <script>
-export let id
-import { storeComments } from '../../scripts/stores.js'
-import Treeview from '../smelte/Treeview'
-import {onMount} from 'svelte'
+    export let id;
+    import { storeComments } from "../../scripts/stores.js";
+    import Treeview from "../smelte/Treeview";
+    import { onMount } from "svelte";
 
-onMount(() => {
-    console.log(`🚀 ~ file: Comments.svelte ~ line 4 ~ storeComments`, $storeComments)
-})
+    onMount(() => {
+        console.log(
+            `🚀 ~ file: Comments.svelte ~ line 4 ~ storeComments`,
+            $storeComments
+        );
+    });
 
-</script> 
+    function getDate(date) {
+        let x = new Date(date).toDateString()
+        return x
+    }
+    function getTime(date) {
+        let x = new Date(date).toLocaleTimeString()
+        return x
+    }
+</script>
 
 <h4>Comments component {id}</h4>
-{#each $storeComments as comment}
-
-{#if comment.snippet.totalReplyCount > 0}
-<div class="grid comment bg-orange-100">
-    <p class="col-start-1 col-span-2">ID: {comment.id}</p>
-    <p class="col-start-3">Date: {comment.snippet.topLevelComment.snippet.publishedAt}</p>
-    <p class="col-start-4 col-span-2">Author: {comment.snippet.topLevelComment.snippet.authorDisplayName}</p>
-    <p class="col-start-6">Replies: {comment.snippet.totalReplyCount}</p>
-    <p class="col-start-7">VideoID: {comment.snippet.topLevelComment.snippet.videoId}</p>
-    <p class="col-start-8 col-span-7">Comment: {comment.snippet.topLevelComment.snippet.textDisplay}</p>
-</div>
-{:else}
-<div class="grid comment bg-green-100">
-    <div class="flex flex-col col-span-2">
-        <p>{comment.snippet.topLevelComment.snippet.authorDisplayName}</p>
-        <p>{comment.snippet.topLevelComment.snippet.publishedAt}</p>
-    </div>
-    <p class="col-start-3 col-span-12">Comment: {comment.snippet.topLevelComment.snippet.textDisplay}</p>
-</div>
-{/if}
+{#each $storeComments as comment, i}
+    {#if comment.snippet.totalReplyCount > 0}
+        <div class="grid comment bg-orange-100">
+            <div class="flex flex-col col-span-2">
+                <p>{i}</p>:
+                <p>
+                    {comment.snippet.topLevelComment.snippet.authorDisplayName}
+                </p>
+                <p>{comment.snippet.topLevelComment.snippet.publishedAt}</p>
+            </div>
+            <p class="col-start-3 col-span-12">
+                {comment.snippet.topLevelComment.snippet.textDisplay}
+            </p>
+            {#each comment.replies.comments as reply}
+                <p class="col-start-3 col-span-12 bg-cyan-100">
+                    <span class="bg-cyan-300">{reply.snippet.authorDisplayName}</span>
+                    <span class="bg-cyan-400">{getDate(reply.snippet.publishedAt)} - {getTime(reply.snippet.publishedAt)}</span>
+                    {reply.snippet.textDisplay}
+                </p>
+            {/each}
+        </div>
+    {:else}
+        <div class="grid comment bg-green-100">
+            <p>{i}</p>:
+            <div class="flex flex-col col-span-2">
+                <p>
+                    {comment.snippet.topLevelComment.snippet.authorDisplayName}
+                </p>
+                <p>{getDate(comment.snippet.topLevelComment.snippet.publishedAt)} - {getTime(comment.snippet.topLevelComment.snippet.publishedAt)}</p>
+            </div>
+            <p class="col-start-3 col-span-12">
+                {comment.snippet.topLevelComment.snippet.textDisplay}
+            </p>
+        </div>
+    {/if}
 {/each}
 
 <style>
-.comment {
- grid-template-columns: repeat(15, 6.5vw);
-}
-    </style>
+    .comment {
+        grid-template-columns: repeat(15, 6.5vw);
+    }
+</style>
